10a0c962187891b5d95abc8373ecb2ea74258b3f
Jonas <jonashdahlhus@gmail.com>
2022-03-15 13:46:19 +0100

    added html, css, and some more functions

--- script/script.js
+++ script/script.js
@@ -1,49 +1,88 @@
-let blinker; // Blinking input marker
-let inputField; // Input field
-let userInput; // User input
-let storage; // Storage
-let option; // Current position
-let route; // Route taken
+'use strict';
+
+/* The blinking square */
+let blinker = document.querySelector(".fa-square-full"); // Grab the square
+// Create a function to make it blink (advanced)
+setInterval(function() {
+    blinker.classList.toggle("blink");
+}, 600);
+// Variables
+let userInputField = document.querySelector(".userInputField"); // Grab the userInputField 
+let userInput = []; // Assign an empty array to it
+
+let ourTerminal = document.querySelector(".terminal"); // Create a variable named ourTerminal and store the .terminal element
+
+let option = 0; // set equal to 0
+let collectPossible = false; // create a variable called collectPossible and set it to false
+let route = []; // set equal to an empty array
+
+let pokemon = ["Pikachu"]; // create a new variable called pokemon and set it equal to an array containing "Pikachu"
+let pokeballCount = 2;// create a new variable called pokeballCount and set it equal to 2
+let berryCount = 0;// create a new variable called berryCount and set it equal to 0
+
+const updatePage = (userInput) => {
+    let newParagraph = document.createElement("p");
+    newParagraph.innerHtml = userInput;
+    ourTerminal.appendChild();
+}
 
 const whatToDo = (userInput) => {
     userInput = userInput.toLowerCase();
     switch (userInput) {
         case "n":
         case "no":
-            console.log("You typed in no");
+        case "nope":
             break;
         
         case "y":
         case "yes":
-            console.log("You typed in yes");
             break;
 
         case "help":
-            console.log("You typed in help");
             break;
         
         case "see storage":
         case "storage":
+            pokemon.forEach(pokemon => {
+                updatePage(pokemon);
+            });
+            updatePage(`${pokeballCount} pokeballs`);
+            updatePage(`${berryCount} berries`);
             console.log("You typed in storage");
             break;
 
         case "right":
-            console.log("You typed in right");
             break;
 
         case "left":
-            console.log("You typed in left");
             break;
 
         case "forward":
-            console.log("You typed in forward");
             break;
-        
-        default:
-            console.log("I didn't understand");
-    }
-}
 
-const functionName = () => {
-    
+        case "back":
+            break;
+
+        case "enter":
+            break;
+
+        case "battle":
+        case "battle leader":
+            break;
+
+        case "catch":
+        case "catch pokemon":
+            break;
+
+        case "berries":
+        case "collect berries":
+            break;
+
+        case "pokeballs":
+        case "collect pokeballs":
+            break;
+
+        default:
+            updatePage("I didn't understand");
+    }
 }
